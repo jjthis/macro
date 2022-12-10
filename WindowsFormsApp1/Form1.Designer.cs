42d6440c140371a7705647f2755ae921b7d89fa7
jjthis <dfd2726@gmail.com>
2022-12-10 22:05:24 +0900

    ad

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(393, 78);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "보내기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 42);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "여기에 방이름";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 208);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "여기에 에러";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(56, 140);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 21);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "여기 내용";
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.key_Down);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(56, 95);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(218, 95);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(560, 300);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApp1{
+    public partial class Form1 : Form{
+
+        [DllImport("user32.dll")]
+        public static extern int FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindowAsync(int hwnd, int howshow);
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(int hwnd);
+        public static int myID=0;
+        public Form1(){
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {}
+
+        private void button1_Click(object sender, EventArgs e){
+            if(textBox2.Text == "kkutu") {
+                ///--and char_length(_id) < 4
+                SendImage(textBox1.Text, "select * from kkutu_en where _id not like '{%} {%}' and _id like '" +
+                    textBox3.Text
+                    + "{%}' order by {(}char_length{(}_id{)} - char_length{(}replace{(}_id,'" +
+                    textBox4.Text
+                    +"', ''{)}{)}{)} desc{f5}");
+            }else SendImage(textBox1.Text, textBox2.Text+"{Enter}");
+            textBox2.Focus();
+        }
+
+
+
+        public void SendImage(string room, string imagePath){
+            if (myID == 0) {
+                myID = FindWindow(null, "Form1");
+            }
+
+            int j = FindWindow(null, room);
+            Console.WriteLine(string.Format("Number in {1}: {0},", j,room));
+            if (j == 0){
+                label2.Text = "방을 찾을 수 없습니다 : " + room;
+                return;
+            }
+
+            ShowWindowAsync(j, 1);
+            Thread.Sleep(20);
+            ShowWindowAsync(j, 1);
+            SetForegroundWindow(j);
+
+            Thread.Sleep(200);
+
+            SendKeys.SendWait(Kor2Eng(imagePath));
+            //SendKeys.Send("{ENTER}"); //카카오톡 화면에 복사를하고 엔터를 입력합니다.
+
+            ShowWindowAsync(myID, 1);
+            Thread.Sleep(20);
+            ShowWindowAsync(myID, 1);
+            SetForegroundWindow(myID);
+
+        }
+
+        public static string Kor2Eng(string kor){
+            if (kor == null) throw new ArgumentNullException();
+            if (kor.Length == 0) throw new ArgumentException();
+
+            var sb = new System.Text.StringBuilder(kor.Length * 2);
+            int ini, vow, und;
+
+            int i = 0;
+            do{
+                if (!Split(kor[i], out ini, out vow, out und))
+                    sb.Append(kor[i]);
+                else{
+                    if (ini != -1) sb.Append(Table[Array.IndexOf<string>(Table, IniS[ini]) + 1]);
+                    if (vow != -1) sb.Append(Table[Array.IndexOf<string>(Table, VolS[vow]) + 1]);
+                    if (und > 0) sb.Append(Table[Array.IndexOf<string>(Table, UndS[und]) + 1]);
+                }
+            } while (++i < kor.Length);
+
+            return sb.ToString();
+        }
+
+        private static bool Split(char src, out int ini, out int vow, out int und){
+            // 원래 초중종 나눔
+            int charCode = Convert.ToInt32(src) - 44032;
+            int i;
+
+            if ((charCode < 0) || (charCode > 11171)){
+                ini = vow = und = -1;
+
+                if ((i = Array.IndexOf<char>(IniC, src)) != -1)
+                    ini = i;
+                else if ((i = Array.IndexOf<char>(VolC, src)) != -1)
+                    vow = i;
+                else if (src != '\0' && (i = Array.IndexOf<char>(UndC, src)) != -1)
+                    und = i;
+            }
+            else{
+                ini = charCode / 588;
+                vow = (charCode % 588) / 28;
+                und = (charCode % 588) % 28;
+            }
+
+            return ini != -1 || vow != -1 || und != -1;
+        }
+
+        private static readonly char[] IniC = { 'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' };
+        private static readonly string[] IniS = { "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ" };
+
+        private static readonly char[] VolC = { 'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ' };
+        private static readonly string[] VolS = { "ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ" };
+
+        private static readonly char[] UndC = { '\0', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' };
+        private static readonly string[] UndS = { "", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ" };
+
+        private static readonly string[] Table ={
+            "ㄱ", "r", "ㄲ", "R",  "ㄳ", "rt",
+            "ㄴ", "s", "ㄵ", "sw", "ㄶ", "sg",
+            "ㄷ", "e", "ㄸ", "E",
+            "ㄹ", "f", "ㄺ", "fr", "ㄻ", "fa", "ㄼ", "fq", "ㄽ", "ft", "ㄾ", "fx", "ㄿ", "fv", "ㅀ", "fg",
+            "ㅁ", "a",
+            "ㅂ", "q", "ㅃ", "Q",  "ㅄ", "qt",
+            "ㅅ", "t", "ㅆ", "T",
+            "ㅇ", "d",
+            "ㅈ", "w",
+            "ㅉ", "W",
+            "ㅊ", "c",
+            "ㅋ", "z",
+            "ㅌ", "x",
+            "ㅍ", "v",
+            "ㅎ", "g",
+            "ㅏ", "k",
+            "ㅐ", "o", "ㅒ", "O",
+            "ㅑ", "i",
+            "ㅓ", "j",
+            "ㅔ", "p", "ㅖ", "P",
+            "ㅕ", "u",
+            "ㅗ", "h", "ㅘ", "hk", "ㅙ", "ho", "ㅚ", "hl",
+            "ㅛ", "y",
+            "ㅜ", "n", "ㅝ", "nj", "ㅞ", "np", "ㅟ", "nl",
+            "ㅠ", "b",
+            "ㅣ", "l",
+            "ㅡ", "m", "ㅢ", "ml",
+        };
+
+        private void key_Down(object sender, KeyEventArgs e){
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, e);
+            }
+        }
+    }
+}
